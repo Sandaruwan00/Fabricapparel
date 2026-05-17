b0b52fd630b5f65c03974d816a56491d93434f94
Sandaruwan00 <sandaruwanekanayake531@gmail.com>
2026-05-18 01:17:39 +0530

    removed duplicate shipment id in assign button

--- view/add-transport.php
+++ view/add-transport.php
@@ -126,7 +126,7 @@ $driverResults = $transportObj->getAllAvailableDrivers();
                                     <a href="#" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#viewModal" onclick="loadshipment('<?php echo $shipment_id; ?>');">
                                         <i class="bi bi-eye-fill"></i> View
                                     </a>
-                                    <a href="#" class="btn btn-success btn-sm" data-bs-toggle="modal" data-bs-target="#assignModal" onclick="loadassign('<?php echo $shipment_id; ?>','<?php echo $Row['district_id']; ?>','<?php echo $shipment_id; ?>');">
+                                    <a href="#" class="btn btn-success btn-sm" data-bs-toggle="modal" data-bs-target="#assignModal" onclick="loadassign('<?php echo $shipment_id; ?>','<?php echo $Row['district_id']; ?>');">
                                         <i class="bi bi-link"></i> Assign
                                     </a>
                                 </td>
