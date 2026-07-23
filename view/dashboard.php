a6ae8c2ba18e5afa89bfcfb2a0ec132fbde79731
Sandaruwan00 <sandaruwanekanayake531@gmail.com>
2026-07-23 11:40:54 +0530

    finalize

--- view/dashboard.php
+++ view/dashboard.php
@@ -26,7 +26,7 @@ $moduleResult = $moduleObj->getAllModules();
         <?php $pageName = "DASHBOARD"; ?>
         <?php include_once "../includes/header_row_includes.php"; ?>
 
-        <div class="row g-4 mt-3">
+        <div class="row g-4 mt-1">
             <?php while ($module_row = $moduleResult->fetch_assoc()) { ?> 
                 <div class="col-md-3">
                     <a href="<?php echo $module_row['module_url']; ?>" class="text-decoration-none text-dark">
